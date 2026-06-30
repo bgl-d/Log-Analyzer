2ab8b227682bfbc1fea6ff0a70a7b47de8c468fb
bgl-d <daborisoglebskiy@gmail.com>
2026-06-30 19:03:11 +0400

    Increased password length

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class Note(db.Model):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
+    password = db.Column(db.String(300))
     first_name = db.Column(db.String(100))
     last_name = db.Column(db.String(100))
     notes = db.relationship('Note')
