d2aa67b62d986ebfd4309a1ec74021bccd7555e7
bgl-d <daborisoglebskiy@gmail.com>
2026-06-30 18:37:02 +0400

    Connect to Neon DB

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,10 @@ from os import path
 from flask_login import LoginManager
 
 db = SQLAlchemy()
-DB_NAME = 'database.db'
 
 def create_app():
     app = Flask(__name__)
     app.config.from_prefixed_env()
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
     from .views import views
